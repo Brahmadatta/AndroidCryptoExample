df84f08425417b04dfbd48069e6017115a71ada3
BrahmaDatta <g.brahmadatta@gmail.com>
2024-02-22 00:08:51 +0530

    added encrypt and decrypt code

--- app/src/main/java/com/example/androidcryptoexample/CryptoManager.kt
+++ app/src/main/java/com/example/androidcryptoexample/CryptoManager.kt
@@ -2,10 +2,13 @@ package com.example.androidcryptoexample
 
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
+import java.io.InputStream
+import java.io.OutputStream
 import java.security.KeyStore
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
 
 class CryptoManager {
 
@@ -15,16 +18,30 @@ class CryptoManager {
 
     private val encryptCipher = Cipher.getInstance(TRANSFORMATION).apply {
         init(
-            Cipher.ENCRYPT_MODE,createKey()
+            Cipher.ENCRYPT_MODE,getKey()
         )
     }
 
+    //Iv - Initialisation vector (Initial state of encryption)
+    private fun getDecryptCipherForIv(iv : ByteArray) : Cipher {
+        return Cipher.getInstance(TRANSFORMATION).apply {
+            init(Cipher.DECRYPT_MODE,getKey(),IvParameterSpec(iv))
+        }
+    }
+
     //Check if key exists
+    private fun getKey() : SecretKey {
+        val existingKey = keyStore.getEntry("secret",null) as? KeyStore.SecretKeyEntry
+        return existingKey?.secretKey ?: createKey()
+    }
 
     private fun createKey() : SecretKey {
         return KeyGenerator.getInstance(ALGORITHM).apply {
             init(
-                KeyGenParameterSpec.Builder()
+                KeyGenParameterSpec.Builder(
+                    "secret",
+                    KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+                )
                     .setBlockModes(BLOCK_MODE)
                     .setEncryptionPaddings(PADDING)
                     .setUserAuthenticationRequired(false)
@@ -32,6 +49,33 @@ class CryptoManager {
                     .build()
             )
         }.generateKey()
+    }
+
+    //encrypt
+    fun encrypt(bytes : ByteArray,outputStream: OutputStream) : ByteArray {
+        val encryptedBytes = encryptCipher.doFinal(bytes)
+        outputStream.use {
+            it.write(encryptCipher.iv.size)
+            it.write(encryptCipher.iv)
+            it.write(encryptedBytes.size)
+            it.write(encryptedBytes)
+        }
+        return encryptedBytes
+    }
+
+    //decrypt
+    fun decrypt(inputStream: InputStream) : ByteArray {
+        return inputStream.use {
+            val ivSize = it.read()
+            val iv = ByteArray(ivSize)
+            it.read(iv)
+
+            val encryptedBytesSize = it.read()
+            val encryptedBytes = ByteArray(encryptedBytesSize)
+            it.read(encryptedBytes)
+
+            getDecryptCipherForIv(iv).doFinal(encryptedBytes)
+        }
     }
 
     companion object {
